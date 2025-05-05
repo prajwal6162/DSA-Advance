c8e642ceea582d99884b28a9b098b5b9fd99c085
Prajwal <prajwalthakur6162@gmail.com>
2025-05-05 19:01:02 +0530

    Auto-push DSA code update on 05-05-2025 19:01:02.90

--- Arrays/Medium/Kadanes algorithm (Follow-up).cpp
+++ Arrays/Medium/Kadanes algorithm (Follow-up).cpp
@@ -1,43 +1,50 @@
 class Solution {
-public:
-    // Function to find maximum sum of subarrays and print the subarray having maximum sum
-    int maxSubArray(vector<int>& nums) {
-    
-        long long maxi = LLONG_MIN; 
-        
-        long long sum = 0;
-        
-        // starting index of current subarray
-        int start = 0; 
-        
-        int ansStart = -1, ansEnd = -1; 
-
-        for (int i = 0; i < nums.size(); i++) {
-
-            if (sum == 0) {
-                start = i;
-            }
-            sum += nums[i]; 
-
-            if (sum > maxi) {
-                maxi = sum;
-                ansStart = start;
-                ansEnd = i;
-            }
+    public:
+        // Function to find maximum sum of subarrays and print the subarray having maximum sum
+        int maxSubArray(vector<int>& nums) {
+            // maximum sum
+            long long maxi = LLONG_MIN; 
             
-            if (sum < 0) {
-                sum = 0;
+            // current sum of subarray
+            long long sum = 0;
+            
+            // starting index of current subarray
+            int start = 0; 
+            
+            // indices of the maximum sum subarray
+            int ansStart = -1, ansEnd = -1; 
+            
+            // Iterate through the array
+            for (int i = 0; i < nums.size(); i++) {
+                
+                // update starting index if sum is reset
+                if (sum == 0) {
+                    start = i;
+                }
+                
+                // add current element to the sum
+                sum += nums[i]; 
+                
+                /* Update maxi and subarray indices
+                if current sum is greater*/
+                if (sum > maxi) {
+                    maxi = sum;
+                    ansStart = start;
+                    ansEnd = i;
+                }
+                
+                // Reset sum to 0 if it becomes negative
+                if (sum < 0) {
+                    sum = 0;
+                }
             }
+            // Printing the subarray
+            cout << "The subarray is: [";
+            for (int i = ansStart; i <= ansEnd; i++) {
+                cout << nums[i] << " ";
+            }
+            cout << "]" << endl;
+            // Return the maximum subarray sum found
+            return maxi;
         }
-        
-        cout << "The subarray is: [";
-        for (int i = ansStart; i <= ansEnd; i++) {
-            cout << nums[i] << " ";
-        }
-        cout << "]" << endl;
-        
-
-        return maxi;
-    }
-};
-
+    };
