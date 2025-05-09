08660b148fef405eda5f7b6c39804f1163f37345
Prajwal <prajwalthakur6162@gmail.com>
2025-05-09 20:26:31 +0530

    Auto-push DSA code update on 09-05-2025 20:26:31.55

--- Arrays/Hard/Majority element 1 (Brute).cpp
+++ Arrays/Hard/Majority element 1 (Brute).cpp
@@ -1,32 +1,15 @@
 class Solution {
     public:
-        // Function to find the majority element in an array
         int majorityElement(vector<int>& nums) {
-            
-            // Size of the given array
-            int n = nums.size();
-            
-            // Iterate through each element of the array
-            for (int i = 0; i < n; i++) {
-                
-                // Counter to count occurrences of nums[i]
-                int cnt = 0; 
-                
-                // Count the frequency of nums[i] in the array
-                for (int j = 0; j < n; j++) {
-                    if (nums[j] == nums[i]) {
-                        cnt++;
+            int n=nums.size();
+            for (int i=0;i<n;i++){
+                int count=0;
+                for(int j=0;j<n;j++){
+                    if(nums[i]==nums[j]){
+                        count++;
                     }
                 }
-                
-                // Check if frequency of nums[i] is greater than n/2
-                if (cnt > (n / 2)) {
-                    // Return the majority element
-                    return nums[i]; 
-                }
-            }
-            
-            // Return -1 if no majority element is found
-            return -1; 
+                if(count> n/2) return nums[i];
+            } 
         }
     };
--- Arrays/Hard/Majority element 1(Better).cpp
+++ Arrays/Hard/Majority element 1(Better).cpp
@@ -1,6 +1,14 @@
 class Solution {
-    public:
-        vector<int> majorityElementTwo(vector<int>& nums) {
-            
+   public:
+       int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map <int, int> mpp;
+        for(int num : nums){
+           mpp[num]++;
+        }   
+        for(auto it : mpp){
+           if(it.second > n/2) return it.first;
         }
-    };
+        return -1;
+       }
+   };
