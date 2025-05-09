f39670322306b8e9ec54ea9ce8520ecc5a4b97c2
Prajwal <prajwalthakur6162@gmail.com>
2025-05-09 20:39:46 +0530

    Auto-push DSA code update on 09-05-2025 20:39:46.54

--- Arrays/Hard/Majority Element 1(Optimal).cpp
+++ Arrays/Hard/Majority Element 1(Optimal).cpp
@@ -1,6 +1,31 @@
 class Solution {
     public:
-        vector<int> majorityElementTwo(vector<int>& nums) {
-            
+    // More Voting Algorithm
+        int majorityElement(vector<int>& nums) {
+            int n=nums.size();
+            int count=0;
+            int ele;
+            for (int i=0;i<n;i++){
+                if(count == 0){
+                    ele=nums[i];
+                    count=1;
+                }
+                else if(nums[i] == ele){
+                    count++;
+                }
+                else{
+                    count--;
+                }
+            }
+    
+            int maxi=0;
+            for (int i=0;i<n;i++){
+                if(nums[i] == ele){
+                    maxi++;
+                }
+                if(maxi > n/2) return ele;
+            }
+    
+            return -1;
         }
     };
