7dae32a1fa0fea13ef70f4f0158e0dbef78b35ff
Prajwal <prajwalthakur6162@gmail.com>
2025-06-29 20:01:11 +0530

    Auto-push DSA code update on 29-06-2025 20:01:11.30

---
+++ Binary Search/Logic Building/First and last occurrence (Linear search).cpp
@@ -0,0 +1,13 @@
+class Solution{
+public:
+    vector<int> searchRange(vector<int> &nums, int target) {
+        int start=-1, end=-1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] == target){
+                if(start==-1) start=i;
+                end=i;
+            }
+        }
+        return {start, end};
+    }
+};
