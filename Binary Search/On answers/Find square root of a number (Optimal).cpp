7e9b6f43806d481869e442f2e84bdf2574006284
Prajwal <prajwalthakur6162@gmail.com>
2025-07-04 12:39:49 +0530

    Auto-push DSA code update on 04-07-2025 12:39:49.36

--- Binary Search/On answers/Find square root of a number (Optimal).cpp
+++ Binary Search/On answers/Find square root of a number (Optimal).cpp
@@ -1,2 +1,22 @@
 //using binary search
 
+class Solution {
+public:
+    long long floorSqrt(long long n)  {
+      typedef long long ll;
+      ll low=1;
+      ll high=n;
+      ll ans=0;
+      while(low <= high){
+        ll mid=(low+high)/2;
+        if((mid*mid) <=n){
+            ans=mid;
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
+      }
+      return ans;
+    }
+};
