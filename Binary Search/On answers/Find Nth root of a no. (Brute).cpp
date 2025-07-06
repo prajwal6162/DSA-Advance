d9b53e3de723cd6adc9db4874138d1b003f54157
Prajwal <prajwalthakur6162@gmail.com>
2025-07-06 19:46:27 +0530

    Auto-push DSA code update on 06-07-2025 19:46:27.27

--- Binary Search/On answers/Find Nth root of a no. (Brute).cpp
+++ Binary Search/On answers/Find Nth root of a no. (Brute).cpp
@@ -1,13 +1,30 @@
 class Solution {
+private:
+  long long Pow(int b, int exp){
+    long long answer=1;
+    long long base=b;
+    while(exp > 0){
+      if(exp%2){
+        exp--;
+        answer=answer*base;
+      }
+      else{
+        exp/=2;
+        base=base*base;
+      }
+    }
+  return answer;
+  }
 public:
   int NthRoot(int N, int M) {
-       for(int i=1;i<=M;i++){
-        long long ans=pow(i,N);
-        if(ans==M*1ll) return i;
-        else if(ans > M*1ll) break;
+       for(int i=0;i<=M;i++){
+        long long  ans=Pow(i, N);
+        if(ans== M*1ll) return i;
+        else if(ans >= M*1ll){
+          break;
+        }
        }
-       return -1;
+      return -1;
     }
 };
-
 //using linear search;
