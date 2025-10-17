676c52f5468d0cef009f4679e829cd1f0eafec8c
Prajwal <prajwalthakur6162@gmail.com>
2025-10-17 19:35:00 +0530

    Auto-push DSA code update on 17-10-2025 19:35:00.93

---
+++ Linked List/Fundamentals (singly)/Insertion at head.cpp
@@ -0,0 +1,28 @@
+/*
+Definition of singly linked list:
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int data1)
+    {
+        val = data1;
+        next = NULL;
+    }
+    ListNode(int data1, ListNode *next1)
+    {
+        val = data1;
+        next = next1;
+    }
+};
+*/
+
+class Solution {
+public:
+    ListNode* insertAtHead(ListNode* &head, int X) {
+        ListNode* temp= new ListNode(X);
+        temp->next=head;
+        head=temp;
+        return head;
+    }
+};
